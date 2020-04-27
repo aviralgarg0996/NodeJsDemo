60664c6384cbdb7231b8ff292c314947b108d032
aviral-fa <aviral.garg@bakingo.com>
2020-04-27 23:41:07 +0530

    first commit

---
+++ notes-app/app.js
@@ -0,0 +1,4 @@
+// const add=require("./utils.js")
+// console.log(add(2,3))
+const getNotes = require("./notes");
+console.log(getNotes());
---
+++ notes-app/utils.js
@@ -0,0 +1,6 @@
+console.log("utile");
+const name = "aviral";
+const add = function (a, b) {
+  return a + b;
+};
+module.exports = add;

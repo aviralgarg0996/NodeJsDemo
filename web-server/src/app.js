c3a4727ee9fc6c016559a23a1732e6f5534ca941
aviral-fa <aviral.garg@bakingo.com>
2020-05-04 09:49:32 +0530

    web-server

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,19 +1,26 @@
+const path=require('path')
 const express=require('express')
 
+const publicDir=path.join(__dirname,'../public')
 const app=express()
 
+app.use(express.static(publicDir))
+
 app.get('',(req,res)=>{
-    res.send('Hello express!')
+    res.send('<h1>Weather</h1>')
 })
 
-app.get('/help',(req,res)=>{
-    res.send('Help Page')
-})
-app.get('/about',(req,res)=>{
-    res.send('About Page')
-})
+// app.get('/help',(req,res)=>{
+//     res.send({name:'Aviral',age:27})
+// })
+// app.get('/about',(req,res)=>{
+//     res.send('<h1>About Page</h1>')
+// })
 app.get('/weather',(req,res)=>{
-    res.send('Your weather')
+    res.send({
+        city:'Los Angeles',
+        weather:'20'
+    })
 })
 
 app.listen(3000,()=>{
